69a016a671fe62ece944ede7bb9cd0a66815056b
MaxLichtneker <maxwil2001@hotmail.com>
2023-10-06 10:27:01 +0200

    video's now get dynamically added

--- Website/WebsiteScripts.js
+++ Website/WebsiteScripts.js
@@ -14,9 +14,11 @@ window.onscroll = function(){
 }   
 
 
-const ProjectPages = ["Malaga jam", "Switch", "Overgrown", "Examen Project", "Endless Runner"];
+const ProjectPages = ["Fix my tattoo", "Switch", "Overgrown", "Examen Project", "Endless Runner"];
 
-var Grid;
+const ProjectVideos = [null, "/Videos/Switchgameplay.mp4",
+                    "/Videos/OvergrownTrailer-720p-1695042157.mp4", 
+                    "/Videos/GameplayFilmpje(19-05-2022).mp4","/Videos/Buildv2.02022.11.19-19.01.37.04.mp4"];
 
 function AddPages(){
 
@@ -25,9 +27,10 @@ function AddPages(){
 
         const PageDivs = document.createElement("div");
         PageDivs.className = "mySlides";
-        PageDivs.id = "Slide";
-
-        document.getElementById("Grid").appendChild(PageDivs);
+        PageDivs.id = ProjectPages[i];
+        
+        var gridElement = document.getElementById("Grid").appendChild(PageDivs);
+        gridElement.innerHTML = "<h2 style='text-align: center; padding-top: 50px;'>"+ProjectPages[i]+"</h2>";
     }
 
     const Slides = document.getElementsByClassName("mySlides")
@@ -39,18 +42,57 @@ function AddPages(){
         const PageDivsGrid = document.createElement("div");
         PageDivsGrid.className = "GridContainer";
         PageDivsGrid.id = "SlideGrid"; 
-
+        
         n.appendChild(PageDivsGrid);
     });
+
+    const Grids = document.getElementsByClassName("GridContainer")
+    const GridArray = Array.from(Grids);
+
+    for(i = 0; i < GridArray.length; i++){
+        var gridId = ProjectPages[i] + "Vid"
+
+        GridArray[i].innerHTML =  "<video class='image' controls>"
+        "</video>"
+
+        const imageClass = document.getElementsByClassName("image");
+        imageClass[i].setAttribute("id", gridId);
+
+        const videoClass = document.getElementById(gridId);
+
+        if(videoClass){
+            videoClass.innerHTML = "<source src = "+ProjectVideos[i]+"></source>";
+        }
+    }
 }
 
-function ProjectPage(ProjectIndex, ProjectTitle, ProjectVideo, ProjectInfoText, ProjectCodeSnippet){
+function ProjectPage(ProjectTitle, ProjectVideo, ProjectInfoText, ProjectCodeSnippet){
+
+    var Page = document.getElementById(ProjectTitle);
+    
+    var pageChild = document.querySelector('.GridContainer');
+    const addOn = ProjectTitle + "video";
+
+    pageChild.innerHTML =  "<video class='image' controls id = "+addOn+">"
+    "</video>"
+
+    var PageVideo = document.getElementsByClassName("image")
+    if(PageVideo){
+        PageVideo.innerHTML = "<source src = "+ProjectVideo+"></source>";
+    }
 }
 
 window.onload = function(){
     AddPages();
 
-    ProjectPage()
+    // //malaga page
+    // ProjectPage("Malaga jam","../Videos/Switch gameplay.mp4",null,null);
+
+    // //switch page
+    // ProjectPage("Switch","/Videos/Switchgameplay.mp4",null,null);
+
+    // //Overgrown page
+    // ProjectPage("Overgrown",'../Videos/OvergrownTrailer-720p-1695042157.mp4',null,null);
 }
 
 
