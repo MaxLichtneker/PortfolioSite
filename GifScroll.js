45941084f8e4cf787c985804df064502a5cec3bf
MaxLichtneker <maxwil2001@hotmail.com>
2023-10-25 15:25:22 +0200

    fixed bug with the gif scroll effect

--- GifScroll.js
+++ GifScroll.js
@@ -1,14 +1,21 @@
 let gifs = ["../ImagesProjects/MalagaJam/MalagaJam_Gif_2.gif", "../ImagesProjects/Switch/SwitchGif_1.gif", "../ImagesProjects/OverGrown/OvergrownGif_1.gif", "../ImagesProjects/EndlessRunner/EndlessRunner_gif_2.gif"];
 
-let index = 0;
+let index = 1;
 
 //loops the gifs
 function VideoLoop(){
+
     document.getElementById("ParralaxId").style.backgroundImage ="url("+gifs[index]+")";
-    index > gifs.length - 1 ? index = 0 : index++;
+    index++;
+
+    if(index > gifs.length){
+        index = 0;
+        document.getElementById("ParralaxId").style.backgroundImage ="url("+gifs[index]+")";
+    }
+
+    console.log(gifs[index]);
 }
 
 window.onload = function(){
-    document.getElementById("ParralaxId").style.backgroundImage ="url("+gifs[0]+")";
     setInterval(VideoLoop, 3000);
-}
+} 
